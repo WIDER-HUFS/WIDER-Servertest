d2ae5daba787667acee3fedf990028d45b01b041
KwonYeonghoo <kupa10000@naver.com>
2025-04-18 17:09:07 +0900

    Spring boot 서버 생성

---
+++ wider/src/test/java/ac/kr/hufs/wider/WiderApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.hufs.wider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
