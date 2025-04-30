179f1a687db0b1db227a9aee4dbc428f439a4cce
KwonYeonghoo <kupa10000@naver.com>
2025-04-30 19:29:27 +0900

    feat: 로그아웃 기능 구현
    - 로그아웃 기능 구현
    - 토큰 삭제 처리 추가
    - 헤더에서 토큰 가져오기
    - 토큰 유효성 검사 추가
    - 토큰 만료 시간 설정

--- wider/src/main/java/ac/kr/hufs/wider/controller/UserController.java
+++ wider/src/main/java/ac/kr/hufs/wider/controller/UserController.java
@@ -1,10 +1,15 @@
 package ac.kr.hufs.wider.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,7 @@ import ac.kr.hufs.wider.model.DTO.SignInDTO;
 import ac.kr.hufs.wider.model.DTO.SignUpDTO;
 import ac.kr.hufs.wider.model.Entity.Users;
 import ac.kr.hufs.wider.model.Service.UserService;
+import ac.kr.hufs.wider.service.JwtService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,6 +26,9 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private JwtService jwtService;
 
     @PostMapping("/signup")
     public Users signup(@RequestBody SignUpDTO signUpDTO) {
@@ -27,9 +36,17 @@ public class UserController {
     }
 
     @PostMapping("/signin")
-    public Users signin(@RequestBody SignInDTO signInDTO) {
-        return userService.signin(signInDTO.getUserId(), signInDTO.getPassword())
+    public ResponseEntity<?> signin(@RequestBody SignInDTO signInDTO) {
+        Users user = userService.signin(signInDTO.getUserId(), signInDTO.getPassword())
                 .orElseThrow(() -> new IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다."));
+        
+        String token = jwtService.generateToken(user.getUserId());
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("user", user);
+        response.put("token", token);
+        
+        return ResponseEntity.ok(response); // HTTP 200 + 응답 본문
     }
     
     @GetMapping("/{userId}")
@@ -42,5 +59,13 @@ public class UserController {
     public String deleteUser(@RequestBody SignInDTO signInDTO) {
         userService.deleteUser(signInDTO.getUserId());
         return "삭제 완료";
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") String token) {
+        // @RequestHeader("Authorization") String token : 헤더에서 토큰을 가져옴 (Bearer 토큰 형식)
+        // 클라이언트에서 토큰을 삭제하는 코드 별도로 필요
+        // 서버에서는 별도의 처리 X
+        return ResponseEntity.ok().body("로그아웃 되었습니다.");
     }
 }
